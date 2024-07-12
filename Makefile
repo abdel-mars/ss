09ebaea216ea0b78f3408ed94713d4569d542fba
abdel-mars <elmahmoudiabderahman@gmail.com>
2024-07-12 19:04:15 +0100

    i

---
+++ Makefile
@@ -0,0 +1,52 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: marvin <marvin@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/28 14:03:46 by abdel-ma          #+#    #+#              #
+#    Updated: 2024/07/05 01:35:21 by marvin           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = so_long
+CC = cc
+FLAGS = -Wall -Werror -Wextra -g
+MLXFLAGS        =       -L ./minilibx -lmlx -Ilmlx -lXext -lX11
+LIBFT = ./libft/libft.a
+LIBFTDIR = ./libft
+MINILIBX_PATH   = ./minilibx
+MINILIBX = $(MINILIBX_PATH)/libmlx.a
+
+SRC =	src/so_long.c \
+	src/game_end.c \
+	src/map_init.c \
+	src/moves.c \
+	src/validate_path.c \
+	src/validate_game.c \
+	src/render_img.c \
+	src/map_size.c \
+
+OBJ = $(SRC:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(MAKE) -C $(MINILIBX_PATH)
+	$(MAKE) -C $(LIBFTDIR)
+	$(CC) $(FLAGS) -lm $(SRC) $(MLXFLAGS) $(LIBFT) -o $(NAME)
+	@echo "[Success] ./so_long created."
+clean:
+	$(MAKE) clean -C $(LIBFTDIR)
+	rm -rf $(OBJ)
+	@echo "[Deleting] object files deleted."
+
+fclean: clean
+	$(MAKE) fclean -C $(LIBFTDIR)
+	rm -rf $(NAME) $(OBJ)
+	@echo "[Deleting] .a deleted."
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ so_long.h
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 13:17:24 by abdel-ma          #+#    #+#             */
+/*   Updated: 2024/07/05 01:34:54 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# define PLAYER "./textures/player.xpm"
+# define COLLECTIBLE "./textures/collectible.xpm"
+# define EXIT "./textures/exit.xpm"
+# define WALL "./textures/wall.xpm"
+# define FLOOR "./textures/floor.xpm"
+# define ONBOX "./textures/on_box.xpm"
+
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define ESC 65307
+
+# include "./minilibx/mlx.h"
+# include "libft/libft.h"
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+typedef struct s_img
+{
+	void	*collectible;
+	void	*player;
+	void	*floor;
+	void	*wall;
+	void	*exit;
+	void	*on_box;
+}	t_img;
+
+typedef struct s_game
+{
+	t_img	img;
+	void	*mlx;
+	void	*win;
+	char	**map;
+	char	**map_floodfill;
+	char	temp;
+	int		line;
+	int		col;
+	int		exit;
+	int		score;
+	int		player;
+	int		player_on_box;
+	int		player_y;
+	int		player_x;
+	int		end_game;
+	int		move;
+}	t_game;
+
+void	check_args(t_game *game, int argc, char **argv);
+void	check_map(t_game *game);
+void	valid_map(t_game *game);
+void	valid_path(t_game *game, int fd);
+void	start_validations(t_game *game, int fd);
+int		key_handler(int x, t_game *game);
+void	get_maps(t_game *game, int fd);
+void	put_images(t_game *game);
+void	put_map(int x, int y, char c, t_game *game);
+int		get_col_size(t_game *game, int fd);
+int		get_line_size(t_game *game, int fd);
+int		render_img(t_game *game);
+int		collectible_counter(t_game *game);
+void	player_position(t_game *game);
+void	free_map(t_game *game);
+void	free_map_floodfill(t_game *game);
+void	free_img(t_game *game);
+void	ft_exit(char *s, t_game *game);
+int		close_window(t_game *game);
+
+#endif
